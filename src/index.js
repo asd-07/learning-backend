5a128920f0a3bd54602e590639c6c06daf85acbf
Anup Singh <anup32633@gmail.com>
2024-04-07 00:13:04 +0530

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path: './env'}) ////cosistency of code khrb kr rha hai tu sale 
+
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+///////////first approach/////////<<<--->>
+// import express from "express";
+
+// const app = express()
+
+// ; ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONOGO_URI}/${DB_NAME}`)
+
+//         app.on("error", (error) => {
+//             console.log("ERRR: ", error);
+//             throw error
+//         })
+
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is running on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.log("ERROR: ", error)
+//         throw err
+//     }
+// })()
