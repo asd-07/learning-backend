10d55f5ce6296e62819be0060fbdcbffe332ec01
Anup Singh <anup32633@gmail.com>
2024-04-07 17:16:17 +0530

    util codessss

--- src/index.js
+++ src/index.js
@@ -8,14 +8,21 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err)
+})
 
 
 
 
 
 
-
-///////////first approach/////////<<<--->>
+///////////first approach/////////<<<--->>>
 // import express from "express";
 
 // const app = express()
---
+++ src/utils/asynHandler.js
@@ -0,0 +1,24 @@
+//promises wala code kaise krna hai dekh lo
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+export {asyncHandler};
+
+
+////ye try catch wala cheez hai 
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
