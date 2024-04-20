7480bf749be7b6d575d9f5aa44667d534839adb6
Anup Singh <anup32633@gmail.com>
2024-04-21 01:01:53 +0530

    add resgister controller

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
 
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
-        console.log("MONGO_DB connection FAILED", error);
+        console.log("MONGO_DB connection nhi ", error);
         process.exit(1)
     }
 }
